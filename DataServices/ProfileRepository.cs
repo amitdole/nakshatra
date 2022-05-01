630290b1ffda2f055d493e2d155bb3dbbce5c1d5
amitdole <36283193+amitdole@users.noreply.github.com>
2022-04-30 21:42:33 -0700

    NAK-14: Hasta Data Services
    NAK-14: Hasta Data Services

--- DataServices/ProfileRepository.cs
+++ DataServices/ProfileRepository.cs
@@ -16,15 +16,25 @@ namespace DataServices
 
         Profile IProfileRepository.GetProfile(int profileId)
         {
-            var data = _configuration.Metadata["UserProfiles"];
-            Profile profile = null;
-            Profile[]? profiles = JsonConvert.DeserializeObject<Profile[]>(data,
-                   new IsoDateTimeConverter { DateTimeFormat = "yyyy-MM-dd" });
-
-            if (profiles != null && profiles.Any())
+            var profile = new Profile();
+            using (var httpClient = new HttpClient())
             {
-                profile = profiles.Where(p => p.Id == profileId).FirstOrDefault();
+                using (var response = httpClient.GetAsync("https://localhost:44324/api/Reservation"))
+                {
+                    string apiResponse = response.Result.Content.ReadAsStringAsync().Result;
+                    profile = JsonConvert.DeserializeObject<Profile>(apiResponse);
+                }
             }
+
+            //var data = _configuration.Metadata["UserProfiles"];
+            //Profile profile = null;
+            //Profile[]? profiles = JsonConvert.DeserializeObject<Profile[]>(data,
+            //       new IsoDateTimeConverter { DateTimeFormat = "yyyy-MM-dd" });
+
+            //if (profiles != null && profiles.Any())
+            //{
+            //    profile = profiles.Where(p => p.Id == profileId).FirstOrDefault();
+            //}
             return profile;
         }
     }
