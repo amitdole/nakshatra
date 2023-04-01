3a137f0ca11449129761b4c6c88428f1fee4d711
amitdole <36283193+amitdole@users.noreply.github.com>
2023-04-01 12:56:42 -0700

    githubactions - azure login integration

--- Core/Api/Model/Email/EmailInfo.cs
+++ Core/Api/Model/Email/EmailInfo.cs
@@ -2,8 +2,8 @@
 
 public class EmailInfo
 {
-    public string Sender { get; set; }
-    public string Receiver { get; set; }
-    public string Subject { get; set; }
-    public string Message { get; set; }
+    public string? Sender { get; set; }
+    public string? Receiver { get; set; }
+    public string? Subject { get; set; }
+    public string? Message { get; set; }
 }
